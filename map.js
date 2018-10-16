9987e6ca38898031c99bbefdfd9c4a871f04cce6
Yardenz <zamir.yarden@gmail.com>
2018-10-15 20:49:03 -0400

    implemented geolocation

---
+++ map.js
@@ -0,0 +1,36 @@
+'use strict' 
+console.log('Loaded map.js') 
+mapboxgl.accessToken = 'pk.eyJ1IjoieWFyZGVueiIsImEiOiJjam5hejlmMGkwMWV6M3BsOWJxcHNhZDJpIn0.yUUMMk4ixslNceyxLrAwzg'
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-73.96216,40.80877],
+    zoom: 12
+})
+let navigation = new mapboxgl.NavigationControl({
+    showCompass: false
+})
+map.addControl(navigation, 'top-left')
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
+})
+map.addControl(scale, 'bottom-right')
+let geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserLocation: true,
+    fitBoundsOptions: {
+    }
+})
+
+map.addControl(geolocate, 'top-left')
+geolocate.on('geolocate', function(event) {
+	let lng = event.coords.longitude
+    let lat = event.coords.latitude
+console.log('geolocated:', lng, lat)
+document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
+})
